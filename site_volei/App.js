d6ff37d704c44724f897b9e8ffb9047649383a4d
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2025-06-08 14:08:40 -0300

    Actualizar App.js

--- site_volei/App.js
+++ site_volei/App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PartidasScreen from './screens/partidas';
+import RegistrarPlacarScreen from './screens/registrarPlacar';
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator initialRouteName="Partidas">
+        <Stack.Screen 
+          name="Partidas" 
+          component={PartidasScreen} 
+          options={{ title: '🏐 Partidas do Torneio' }} 
+        />
+        <Stack.Screen 
+          name="RegistrarPlacar" 
+          component={RegistrarPlacarScreen} 
+          options={{ title: '📝 Registrar Placar' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
